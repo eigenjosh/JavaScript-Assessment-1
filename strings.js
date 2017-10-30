681f4fec4eef5124b6e35140cd96a1ad574770cf
Josh Eigen <josh.eigen@gmail.com>
2017-10-30 07:58:19 -0700

    strings complete

--- strings.js
+++ strings.js
@@ -20,20 +20,48 @@ function upperCase(str) {
             } else {
                 out += 'i'
 
-            } } else {
-                
-                out += str[n]
             }
+        } else {
+
+            out += str[n]
         }
-        return out
     }
+    return out
+}
 
 
 //2. The history of JavaScript is interesting but how many times does the letter a appear? Find a way to count both upper and lower case A's
 
 var jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
 
+function charCountA(str) {
+    var lowerA = 0
+    var upperA = 0
+
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i];
+        if (char == 'a') {
+            lowerA++
+        } if (char == 'A') {
+            upperA++
+        }
+
+    }
+
+    console.log(lowerA)
+    console.log(upperA)
+}
+
+
+
 //3. Given the following sentence: Uppercase the entire word scream each time it appears in the sentence 
 //I SCREAM, you SCREAM, we all SCREAM for icecream
 
 var text = 'I scream, you scream, we all scream for icecream';
+
+function justScreaming(str) {
+    for (i = 0; i < 3; i++) {
+        text = text.replace("scream", "SCREAM")
+    }
+    console.log(text)
+}
